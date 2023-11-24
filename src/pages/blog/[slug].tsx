c5b64dd68da9c736657ca10e252e02baead1533a
Asaduzzama-n <asad_uzzaman@outlook.com>
2023-11-24 16:14:15 +0600

    added details page with dynamic data fetch using getStaticProps and getStaticPaths

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,12 +1,26 @@
 import RootLayout from "@/components/Layouts/RootLayout";
+import Image from "next/image";
 import { useRouter } from "next/router";
 import React, { ReactElement } from "react";
 
-const BlogDetails = () => {
+const BlogDetails = ({blog}:any) => {
   const router = useRouter();
+  
   return (
     <div>
-      <h1>This is blog ID: {router.query.slug}</h1>
+      <div className="flex justify-around my-10 min-h-screen">
+        <div className="w-1/3">
+          <Image className="rounded-md" src={blog?.image_url} height={400} width={500} alt=""></Image>
+        </div>
+        <div className="w-2/4">
+          <h1 className="text-5xl font-bold mb-5">{blog?.title}</h1>
+          <div className="my-4 h-1 bg-black"></div>
+          <p className="text-md font-semibold my-2">Published: {blog?.publish_date}</p>
+          <p className="text-md  my-2"> {blog?.content}</p>
+
+        </div>
+      </div>
+
     </div>
   );
 };
@@ -16,3 +30,28 @@ export default BlogDetails;
 BlogDetails.getLayout = function getLayout(page: ReactElement) {
   return <RootLayout>{page}</RootLayout>;
 };
+
+
+export const getStaticPaths =async () => {
+  const res = await fetch(` http://localhost:5000/blogs`)
+  const data = await res.json();
+
+  const paths = data.map((blog:any) => ({
+    params: {slug: blog.id}
+  }))
+
+  return {paths, fallback: true}
+
+}
+
+export const getStaticProps = async (context:any) => {
+  const {params} = context;
+  const res = await fetch(` http://localhost:5000/blogs/${params.slug}`)
+  const data = await res.json();
+
+  return {
+    props:{
+      blog: data,
+    }
+  }
+}
